ec423dceb5eb7efa94896f09807f6af89a7b620f
ttcom <1902305073@qq.com>
2019-08-22 17:19:34 +0800

    1719

---
+++ src/directives/clickOutside.js
@@ -0,0 +1,32 @@
+
+// const listener = function (e) {
+//   return (el, bindings) => {
+//     if (e.target === el || el.contains(e.target)) {
+//       return;
+//     }
+//     bindings.value(); // close事件
+//   };
+// };
+
+export default { // 指令是一个方法  指令有自己的生命周期
+  // bind update
+
+  clickOutside: {
+    inserted(el, bindings) { // el真实的dom元素
+	     console.log('el',el);
+		 console.log('bindings',bindings);
+	
+      el.listener = function listener(e) {   //el监听函数
+        if (e.target === el || el.contains(e.target)) {
+          return;
+        }
+        bindings.value(); // 不满足条件时触发close事件
+      };
+      document.addEventListener('click', el.listener);
+    },
+    unbind(el) {
+	  console.log("组件unbind")	
+      document.removeEventListener('click', el.listener);
+    },
+  },
+};
